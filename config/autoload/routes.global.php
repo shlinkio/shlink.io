a8c6014ed1836060ce839e27543addb9576be3b9
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-08-04 17:22:41 +0200

    Fixed some styles

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -11,6 +11,12 @@ return [
             'middleware' => Action\TemplateAction::class,
             'allowed_methods' => ['GET'],
         ],
+        [
+            'name' => 'api-docs-section',
+            'path' => '/api-docs/{template}',
+            'middleware' => Action\TemplateAction::class,
+            'allowed_methods' => ['GET'],
+        ],
     ],
 
 ];
