dcf10a402b29aea18d4ef3c77cd8db279dcd3257
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-08-29 13:08:54 +0200

    Updated version

--- config/autoload/templates.global.php
+++ config/autoload/templates.global.php
@@ -14,7 +14,7 @@ return [
             // extension service names or instances
         ],
         'globals' => [
-            'version' => '1.2.1',
+            'version' => '1.2.2',
         ],
     ],
 ];
