78bccd66844734ef886f56d07325b4b4cf57dc55
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-03-19 20:49:23 +0100

    Replaced all the global configuration by zend expressive packages config providers

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -1,57 +1,20 @@
 <?php
 
 use Doctrine\Common\Cache;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Shlinkio\Website\Action;
 use Shlinkio\Website\Factory\CacheDelegator;
 use Shlinkio\Website\Middleware;
 use Shlinkio\Website\Twig\Extension\RouteResultExtension;
 use Zend\Expressive;
-use Zend\Expressive\Container;
-use Zend\Expressive\Helper;
-use Zend\Expressive\Middleware\ErrorResponseGenerator;
-use Zend\Expressive\Response;
-use Zend\Expressive\Router;
-use Zend\Expressive\Template\TemplateRendererInterface;
-use Zend\Expressive\Twig\TwigEnvironmentFactory;
-use Zend\Expressive\Twig\TwigRendererFactory;
-use Zend\HttpHandlerRunner\Emitter\EmitterInterface;
-use Zend\HttpHandlerRunner\RequestHandlerRunner;
-use Zend\Stratigility\Middleware\ErrorHandler;
 
 return [
 
     'dependencies' => [
         'invokables' => [
-            Helper\ServerUrlHelper::class => Helper\ServerUrlHelper::class,
-            Router\RouterInterface::class => Router\FastRouteRouter::class,
             Cache\ApcuCache::class => Cache\ApcuCache::class,
             RouteResultExtension::class => RouteResultExtension::class,
         ],
         'factories' => [
-            Expressive\Application::class => Container\ApplicationFactory::class,
-            Expressive\MiddlewareFactory::class => Container\MiddlewareFactoryFactory::class,
-            Expressive\MiddlewareContainer::class => Container\MiddlewareContainerFactory::class,
-            Helper\UrlHelper::class => Helper\UrlHelperFactory::class,
-            Helper\ServerUrlMiddleware::class => Helper\ServerUrlMiddlewareFactory::class,
-            Helper\UrlHelperMiddleware::class => Helper\UrlHelperMiddlewareFactory::class,
-            TemplateRendererInterface::class => TwigRendererFactory::class,
-            \Twig_Environment::class => TwigEnvironmentFactory::class,
-            ErrorHandler::class => Container\ErrorHandlerFactory::class,
-            ErrorResponseGenerator::class => Container\ErrorResponseGeneratorFactory::class,
-            'Zend\Expressive\ApplicationPipeline' => Container\ApplicationPipelineFactory::class,
-            Router\RouteCollector::class => Router\RouteCollectorFactory::class,
-            RequestHandlerRunner::class => Container\RequestHandlerRunnerFactory::class,
-            EmitterInterface::class => Container\EmitterFactory::class,
-            ServerRequestInterface::class => Container\ServerRequestFactoryFactory::class,
-            ResponseInterface::class => Container\ResponseFactoryFactory::class,
-            Response\ServerRequestErrorResponseGenerator::class
-                => Container\ServerRequestErrorResponseGeneratorFactory::class,
-            Router\Middleware\RouteMiddleware::class => Router\Middleware\RouteMiddlewareFactory::class,
-            Router\Middleware\DispatchMiddleware::class => Router\Middleware\DispatchMiddlewareFactory::class,
-            Expressive\Handler\NotFoundHandler::class => Container\NotFoundHandlerFactory::class,
-
             // Actions
             Action\TemplateAction::class => Action\TemplateActionFactory::class,
 
@@ -67,7 +30,7 @@ return [
                 CacheDelegator::class,
             ],
             Expressive\Application::class => [
-                Container\ApplicationConfigInjectionDelegator::class,
+                Expressive\Container\ApplicationConfigInjectionDelegator::class,
             ],
         ],
     ],
--- config/config.php
+++ config/config.php
@@ -2,7 +2,13 @@
 declare(strict_types=1);
 
 use Zend\ConfigAggregator;
+use Zend\Expressive;
 
 return (new ConfigAggregator\ConfigAggregator([
+    Expressive\ConfigProvider::class,
+    Expressive\Router\ConfigProvider::class,
+    Expressive\Router\FastRouteRouter\ConfigProvider::class,
+    Expressive\Twig\ConfigProvider::class,
+    Expressive\Helper\ConfigProvider::class,
     new ConfigAggregator\ZendConfigProvider('config/autoload/{{,*.}global,{,*.}local}.php'),
 ], 'data/cache/app_config.php'))->getMergedConfig();
