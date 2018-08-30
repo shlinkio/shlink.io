99644cfc64507c58b7954f3f029ec7d56b7c6849
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-08-30 19:26:35 +0200

    Added missing function imports

--- config/autoload/donate.global.php
+++ config/autoload/donate.global.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace Shlink\Website;
 
+use function sprintf;
+use function http_build_query;
+
 return [
 
     'donate' => [
-        'url' => \sprintf('https://www.paypal.com/cgi-bin/webscr?%s', http_build_query([
+        'url' => sprintf('https://www.paypal.com/cgi-bin/webscr?%s', http_build_query([
             'cmd' => '_donations',
             'business' => 'alejandrocelaya@gmail.com',
             'currency_code' => 'EUR',
