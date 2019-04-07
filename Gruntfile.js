367aae432a458047c8e5337aec41cd993aaf9bb2
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-04-07 21:01:29 +0200

    Added docker image config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,28 @@
-module.exports = function(grunt) {
+module.exports = grunt => {
 
-    // Get command line arguments
-    var cssFile = grunt.option('css-file') || 'public/assets/css/main.min.css',
-        jsFile = grunt.option('js-file') || 'public/assets/js/main.min.js',
-        cssFilesTemplate = {},
-        jsFilesTemplate = {},
-        currentTimestamp = new Date().getTime();
+    const cssFile = grunt.option('css-file') || 'public/assets/css/main.min.css';
+    const jsFile = grunt.option('js-file') || 'public/assets/js/main.min.js';
+    const cssFilesTemplate = {};
+    const jsFilesTemplate = {};
+    const currentTimestamp = new Date().getTime();
 
-    cssFilesTemplate[cssFile]   = [
+    cssFilesTemplate[cssFile] = [
         'public/assets/css/font-awesome.min.css',
         'public/assets/css/highlightjs-github.min.css',
-        'public/assets/css/main.css'
+        'public/assets/css/main.css',
     ];
-    jsFilesTemplate[jsFile]     = [
+    jsFilesTemplate[jsFile] = [
         'public/assets/js/jquery.min.js',
         'public/assets/js/jquery.scrolly.min.js',
         'public/assets/js/highlight.pack.js',
         'public/assets/js/skel.min.js',
-        'public/assets/js/main.js'
+        'public/assets/js/main.js',
     ];
 
-    // Project configuration.
     grunt.initConfig({
 
-        // Load configuration
         pkg: grunt.file.readJSON('package.json'),
 
-        // Minify app JS files into one file dropping console object
         uglify: {
             options: {
                 compress: {
@@ -38,7 +34,6 @@ module.exports = function(grunt) {
             }
         },
 
-        // Minify CSS files
         cssmin: {
             main: {
                 files: cssFilesTemplate
@@ -48,7 +43,7 @@ module.exports = function(grunt) {
         processhtml: {
             main: {
                 files: {
-                    'templates/layout.html.twig': ['templates/layout.html.twig']
+                    'templates/layout.phtml': ['templates/layout.phtml']
                 }
             }
         },
@@ -58,7 +53,7 @@ module.exports = function(grunt) {
                 options: {
                     replacements: [{
                         pattern: /(.js|.css)\?v/ig,
-                        replacement: '$1?v=' + currentTimestamp
+                        replacement: `$1?v=${currentTimestamp}`
                     }]
                 },
                 files : {
@@ -69,13 +64,11 @@ module.exports = function(grunt) {
 
     });
 
-    // Load the plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-processhtml');
 
-    // Default task.
     grunt.registerTask('default', ['uglify', 'cssmin', 'processhtml', 'string-replace']);
 
 };
