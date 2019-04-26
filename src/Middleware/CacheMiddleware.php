4737160196bb137e2d585136fd87f58964bd64cd
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-04-26 21:20:05 +0200

    Ensured short code generation action is never cached

--- src/Middleware/CacheMiddleware.php
+++ src/Middleware/CacheMiddleware.php
@@ -1,16 +1,23 @@
 <?php
+declare(strict_types=1);
+
 namespace Shlinkio\Website\Middleware;
 
 use Doctrine\Common\Cache\Cache;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 
+use function in_array;
+
 class CacheMiddleware implements MiddlewareInterface
 {
+    private const EXCLUDED_PATHS = [
+        '/short-code-chars'
+    ];
+
     /**
      * @var Cache
      */
@@ -21,26 +28,21 @@ class CacheMiddleware implements MiddlewareInterface
         $this->cache = $cache;
     }
 
-    /**
-     * Process an incoming server request and return a response, optionally delegating
-     * to the next middleware component to create the response.
-     *
-     * @param Request $request
-     * @param RequestHandlerInterface $handler
-     *
-     * @return Response
-     */
-    public function process(Request $request, RequestHandlerInterface $handler): ResponseInterface
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $uri = $request->getUri();
-        $cacheKey= sprintf('%s://%s/%s', $uri->getScheme(), $uri->getAuthority(), $uri->getPath());
+        $path = $uri->getPath();
+        if (in_array($path, self::EXCLUDED_PATHS, true)) {
+            return $handler->handle($request);
+        }
+
+        $cacheKey= sprintf('%s://%s/%s', $uri->getScheme(), $uri->getAuthority(), $path);
         if ($this->cache->contains($cacheKey)) {
             return new HtmlResponse($this->cache->fetch($cacheKey));
         }
 
-        /** @var Response $resp */
         $resp = $handler->handle($request);
-        $this->cache->save($cacheKey, $resp->getBody()->__toString());
+        $this->cache->save($cacheKey, (string) $resp->getBody());
         return $resp;
     }
 }
