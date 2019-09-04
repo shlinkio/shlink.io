7d913814d3a04f8bf9825134b4501cad5c02177b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-09-04 16:03:41 +0200

    Added first tests as POC

---
+++ test/components/FakeBrowser.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import FakeBrowser from '../../components/FakeBrowser';
+
+describe('<FakeBrowser />', () => {
+  afterEach(cleanup);
+
+  it('properly renders inBrowser and children', () => {
+    const { getByText } = render(<FakeBrowser inBrowser={<span>Inside the browser</span>}>My contents</FakeBrowser>);
+
+    expect(getByText('Inside the browser')).toBeInTheDocument();
+    expect(getByText('My contents')).toBeInTheDocument();
+  });
+});
