ba3a954756d2c13dfc22a346b6dbe3de24777ded
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-02-24 20:54:08 +0100

    Fixed tests

--- test/components/Feature.spec.tsx
+++ test/components/Feature.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 import Feature from '../../src/components/Feature';
 
 describe('<Feature />', () => {
   const theTitle = 'The title';
-  const theIcon = 'the-icon';
+  const theIcon = faBook;
 
   afterEach(cleanup);
 
@@ -14,6 +15,6 @@ describe('<Feature />', () => {
 
     expect(getByText('This is the feature')).toBeInTheDocument();
     expect(getByText(theTitle)).toBeInTheDocument();
-    expect(container.querySelector(`.${theIcon}`)).toBeInTheDocument();
+    expect(container.querySelector('.fa-book')).toBeInTheDocument();
   });
 });
