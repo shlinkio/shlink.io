24b8ab93b65da99640eced58c8ca31b406d8732c
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-02-25 20:08:38 +0100

    Added title anchors in docs

--- src/utils/markdownUtils.tsx
+++ src/utils/markdownUtils.tsx
@@ -1,46 +1,81 @@
 import React, { FunctionComponent } from 'react';
 import Highlight from 'react-highlight';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 import Link from '../components/Link';
 
-const childrenToId = (children: string) => children.replace(/ /g, '-').toLowerCase();
-const TitleWithAnchor: FunctionComponent<{ title: string }> = ({ children, title }) => (
-  <React.Fragment>
-    <a className="hidden-title-anchor" id={childrenToId(title.toString())} />
-    {children}
-  </React.Fragment>
-);
+const TitleAnchors: FunctionComponent<{ title: string }> = ({ title }) => {
+  const link = title.replace(/ /g, '-').toLowerCase();
+
+  return (
+    <React.Fragment>
+      <a className="hidden-title-anchor" id={link} />
+      <a href={`#${link}`} className="anchor"><FontAwesomeIcon icon={faLink} /></a>
+    </React.Fragment>
+  );
+};
 
 const a = (props: any) => <Link {...props} />;
 const code = (props: any) => <Highlight {...props} />;
 const h1 = (props: any) => {
-  const { children } = props;
+  const { children, ...rest } = props;
 
-  return <TitleWithAnchor title={children}><h1 {...props} /></TitleWithAnchor>;
+  return (
+    <h1 {...rest}>
+      <TitleAnchors title={children} />
+      {children}
+    </h1>
+  );
 };
 const h2 = (props: any) => {
-  const { children } = props;
+  const { children, ...rest } = props;
 
-  return <TitleWithAnchor title={children}><h2 {...props} /></TitleWithAnchor>;
+  return (
+    <h2 {...rest}>
+      <TitleAnchors title={children} />
+      {children}
+    </h2>
+  );
 };
 const h3 = (props: any) => {
-  const { children } = props;
+  const { children, ...rest } = props;
 
-  return <TitleWithAnchor title={children}><h3 {...props} /></TitleWithAnchor>;
+  return (
+    <h3 {...rest}>
+      <TitleAnchors title={children} />
+      {children}
+    </h3>
+  );
 };
 const h4 = (props: any) => {
-  const { children } = props;
+  const { children, ...rest } = props;
 
-  return <TitleWithAnchor title={children}><h4 {...props} /></TitleWithAnchor>;
+  return (
+    <h4 {...rest}>
+      <TitleAnchors title={children} />
+      {children}
+    </h4>
+  );
 };
 const h5 = (props: any) => {
-  const { children } = props;
+  const { children, ...rest } = props;
 
-  return <TitleWithAnchor title={children}><h5 {...props} /></TitleWithAnchor>;
+  return (
+    <h5 {...rest}>
+      <TitleAnchors title={children} />
+      {children}
+    </h5>
+  );
 };
 const h6 = (props: any) => {
-  const { children } = props;
+  const { children, ...rest } = props;
 
-  return <TitleWithAnchor title={children}><h6 {...props} /></TitleWithAnchor>;
+  return (
+    <h6 {...rest}>
+      <TitleAnchors title={children} />
+      {children}
+    </h6>
+  );
 };
 
 export const markdownComponents = { a, code, h1, h2, h3, h4, h5, h6 };
