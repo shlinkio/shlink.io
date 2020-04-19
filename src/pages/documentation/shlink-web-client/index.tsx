149108a2d91376a09617b6c4eecc2a4014e806ae
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-04-19 19:42:02 +0200

    Hidden breadcrumb in mobile resolutions

--- src/pages/documentation/shlink-web-client/index.tsx
+++ src/pages/documentation/shlink-web-client/index.tsx
@@ -1,9 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import ShlinkWebClientContent from '../../../content/shlink-web-client/index.mdx';
-import Documentation from '../index';
+import Breadcrumb from '../../../components/Breadcrumb';
+import Documentation, { Item } from '../index';
+
+const breadcrumbItems: Item[] = [{ text: 'Docs', link: '/documentation' }];
 
 const ShlinkWebClient: FunctionComponent = () => (
   <Documentation>
+    <Breadcrumb prevElements={breadcrumbItems}>Shlink web client</Breadcrumb>
     <ShlinkWebClientContent />
   </Documentation>
 );
