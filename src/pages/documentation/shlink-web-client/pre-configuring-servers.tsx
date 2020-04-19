9422b4bbd120d05aba361f92d597052372516d9f
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-04-19 19:36:32 +0200

    Added more documentations on how to work with shlink-web-client

---
+++ src/pages/documentation/shlink-web-client/pre-configuring-servers.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+import PreConfiguringServersContent from '../../../content/shlink-web-client/pre-configuring-servers.mdx';
+import Documentation, { Item } from '../index';
+import Breadcrumb from '../../../components/Breadcrumb';
+
+const breadcrumbItems: Item[] = [
+  { text: 'Docs', link: '/documentation' },
+  { text: 'Shlink web client', link: '/documentation/shlink-web-client' },
+];
+
+const PreConfiguringServers: FunctionComponent = () => (
+  <Documentation>
+    <Breadcrumb prevElements={breadcrumbItems}>Pre-configuring servers</Breadcrumb>
+    <PreConfiguringServersContent />
+  </Documentation>
+);
+
+export default PreConfiguringServers;
