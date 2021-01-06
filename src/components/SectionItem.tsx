9912bc362cfe2ef4f3f550df2512ad4067301c75
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-01-06 10:08:10 +0100

    Fixed shlink-web-client video in some devices, and added placeholder image for others

--- src/components/SectionItem.tsx
+++ src/components/SectionItem.tsx
@@ -13,9 +13,7 @@ const SectionItem: FunctionComponent<MainItemProps> = ({ title, children, block,
       <div className={classNames('col-12 col-md-5 mb-3 mb-md-0 align-self-center', { 'order-md-1 pl-md-5': reverse })}>
         <div className="content pr-5">
           <h3 className="heading">{title}</h3>
-          <div className="desc">
-            <p>{children}</p>
-          </div>
+          <div className="desc">{children}</div>
         </div>
       </div>
       <div className="col-12 col-md-7">{block}</div>
