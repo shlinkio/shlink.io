bfb67d3bc3e8f652f9d30160da82663adb1e531b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-07-07 19:11:11 +0200

    Updated to next.js 11 and latest react and next.js plugins

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const withPlugins = require('next-compose-plugins');
 const withFonts = require('next-fonts');
 const withMDX = require('@next/mdx')();
-const withSass = require('@zeit/next-sass');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = withPlugins([ withSass, withFonts, withMDX ], {
+module.exports = withPlugins([ withFonts, withMDX ], {
   enableSvg: true,
   trailingSlash: true, // Makes pages to be exported as index.html files
   env: { // Any of these vars can be accessed as process.env.{name}
@@ -15,8 +14,8 @@ module.exports = withPlugins([ withSass, withFonts, withMDX ], {
     publicDirectory: true, // Makes all content in public dir to be served statically from the root
   },
   webpack(config) {
-    config.optimization.minimizer = config.optimization.minimizer || [];
-    config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+    config.plugins = config.plugins || [];
+    config.plugins.push(new OptimizeCSSAssetsPlugin({}));
 
     return config;
   },
--- src/components/DocsSearch.tsx
+++ src/components/DocsSearch.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent, useLayoutEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import './DocsSearch.scss';
 
 const DocsSearch: FunctionComponent = () => {
   const inputRef = useRef<HTMLInputElement>(null);
