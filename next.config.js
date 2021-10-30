5b8f5a00f326ea7a54bc294193ac6944b4837114
Alejandro Celaya <alejandrocelaya@gmail.com>
2021-10-30 11:30:29 +0200

    Migrated minification from webpack to swc

--- next.config.js
+++ next.config.js
@@ -1,23 +1,17 @@
 const withPlugins = require('next-compose-plugins');
 const withFonts = require('next-fonts');
 const withMDX = require('@next/mdx')();
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = withPlugins([ withFonts, withMDX ], {
   enableSvg: true,
   trailingSlash: true, // Makes pages to be exported as index.html files
+  swcMinify: true, // This will make compiled assets to be minified via SWC instead of webpack plugin
   env: { // Any of these vars can be accessed as process.env.{name}
     donateUrl: 'https://slnk.to/donate',
     ga: 'UA-38351554-5',
   },
   experimental: {
     publicDirectory: true, // Makes all content in public dir to be served statically from the root
-  },
-  webpack(config) {
-    config.plugins = config.plugins || [];
-    config.plugins.push(new CssMinimizerPlugin({}));
-
-    return config;
   },
   eslint: {
     ignoreDuringBuilds: true,
