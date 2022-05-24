2cdbb63f86246ed305460ffd20c6a9a33baa002b
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-05-24 19:31:15 +0200

    Updated tests to how react testing library works now

--- test/components/SectionHeader.spec.tsx
+++ test/components/SectionHeader.spec.tsx
@@ -1,14 +1,11 @@
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
 import SectionHeader from '../../src/components/SectionHeader';
 
 describe('<SectionHeader />', () => {
-  afterEach(cleanup);
-
   it('properly renders provided children inside a title', () => {
-    const { getByText } = render(<SectionHeader title="Some title">This is the subtitle</SectionHeader>);
+    render(<SectionHeader title="Some title">This is the subtitle</SectionHeader>);
 
-    expect(getByText('This is the subtitle')).toBeInTheDocument();
-    expect(getByText('Some title')).toBeInTheDocument();
+    expect(screen.getByText('This is the subtitle')).toBeInTheDocument();
+    expect(screen.getByText('Some title')).toBeInTheDocument();
   });
 });
