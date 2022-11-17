4968a4d644e1f8df8f1f72c6be34ac8b209d89bb
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-11-17 21:34:57 +0100

    Added link to mastodon profile

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import classNames from 'classnames';
 import { ExternalLink } from 'react-external-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faPaypal, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faPaypal, faTwitter, faMastodon } from '@fortawesome/free-brands-svg-icons';
 
 interface SocialListProps {
   type: 'inline' | 'unstyled';
@@ -19,6 +19,11 @@ const SocialList: FC<SocialListProps> = ({ type, className }) => (
     <li className="list-inline-item">
       <ExternalLink href="https://twitter.com/shlinkio">
         <FontAwesomeIcon icon={faTwitter} fixedWidth />
+      </ExternalLink>
+    </li>
+    <li className="list-inline-item">
+      <ExternalLink href="https://fosstodon.org/@shlinkio">
+        <FontAwesomeIcon icon={faMastodon} fixedWidth />
       </ExternalLink>
     </li>
     <li className="list-inline-item">
