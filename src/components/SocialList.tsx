a732bf430f525871ff0f871f8be1b423d0517a77
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-04-07 13:11:09 +0200

    Use actual bluesky logo

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,5 +1,4 @@
-import { faGithub, faMastodon, faPaypal, faXTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faBluesky, faGithub, faMastodon, faPaypal, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import type { FC } from 'react';
@@ -13,32 +12,27 @@ interface SocialListProps {
 export const SocialList: FC<SocialListProps> = ({ type, className }) => (
   <ul className={classNames(`social-list list-${type}`, className)}>
     <li className="list-inline-item">
-      <ExternalLink href="https://github.com/shlinkio/shlink" title="GitHub">
-        <span className="sr-only">GitHub</span>
+      <ExternalLink href="https://github.com/shlinkio/shlink" title="GitHub" aria-label="GitHub">
         <FontAwesomeIcon icon={faGithub} fixedWidth />
       </ExternalLink>
     </li>
     <li className="list-inline-item">
-      <ExternalLink href="https://fosstodon.org/@shlinkio" rel="me" title="Mastodon">
-        <span className="sr-only">Mastodon</span>
+      <ExternalLink href="https://fosstodon.org/@shlinkio" rel="me" title="Mastodon" aria-label="Mastodon">
         <FontAwesomeIcon icon={faMastodon} fixedWidth />
       </ExternalLink>
     </li>
     <li className="list-inline-item">
-      <ExternalLink href="https://twitter.com/shlinkio" title="Twitter">
-        <span className="sr-only">Twitter</span>
+      <ExternalLink href="https://twitter.com/shlinkio" title="Twitter" aria-label="Twitter">
         <FontAwesomeIcon icon={faXTwitter} fixedWidth />
       </ExternalLink>
     </li>
     <li className="list-inline-item">
-      <ExternalLink href="https://bsky.app/profile/shlinkio.bsky.social" title="Bluesky">
-        <span className="sr-only">Bluesky</span>
-        <FontAwesomeIcon icon={faCloud} fixedWidth />
+      <ExternalLink href="https://bsky.app/profile/shlinkio.bsky.social" title="Bluesky" aria-label="Bluesky">
+        <FontAwesomeIcon icon={faBluesky} fixedWidth />
       </ExternalLink>
     </li>
     <li className="list-inline-item">
-      <ExternalLink href="https://slnk.to/donate" title="Donate">
-        <span className="sr-only">Donate</span>
+      <ExternalLink href="https://slnk.to/donate" title="Donate" aria-label="Donate">
         <FontAwesomeIcon icon={faPaypal} fixedWidth />
       </ExternalLink>
     </li>
