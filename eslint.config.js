2e1b28c36049078d4e56f8fda1e46736d2681ac3
JounQin <admin@1stg.me>
2025-03-30 00:40:06 +0800

    fix: config import plugin correctly

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,29 @@
 import shlink from '@shlinkio/eslint-config-js-coding-standard';
+import { defineConfig } from 'eslint/config';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
 /* eslint-disable-next-line no-restricted-exports */
-export default [
+export default defineConfig(
   ...shlink,
-  ...eslintPluginAstro.configs.recommended,
-
+  {
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+    },
+  },
   {
     files: ['**/*.astro'],
+    extends: eslintPluginAstro.configs['flat/recommended'],
+    settings: {
+      'import/parsers': {
+        'astro-eslint-parser': ['.astro'],
+        espree: ['.js', '.mjs'],
+      },
+    },
     rules: {
       // Linter is throwing a false positive on the usage of `class` property when it should be className, but in astro
       // files, `class` is correct
       'react/no-unknown-property': ['error', { ignore: ['class'] }],
-
-      // Disable eslint-plugin-import rules in astro files, as they throw an error
-      // See https://github.com/import-js/eslint-plugin-import/issues/3058
-      'import/no-unresolved': 'off',
-      'import/named': 'off',
-      'import/namespace': 'off',
-      'import/default': 'off',
-      'import/export': 'off',
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-duplicates': 'off',
     },
   },
-];
+);
